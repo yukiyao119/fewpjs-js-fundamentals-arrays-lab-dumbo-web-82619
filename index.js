ef6135a2ad3d40c3f40b22206be97557f359739b
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-10-07 18:59:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
-const append;
-const prepend;
-const removeLast;
-const removeFirst;
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
