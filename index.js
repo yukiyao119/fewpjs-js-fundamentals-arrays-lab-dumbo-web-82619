3b0e4d7c2b01107c5f7810ba277903f22e06767d
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-10-07 19:02:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const append = ["Milo", "Otis", "Garfield"];
 const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
+
